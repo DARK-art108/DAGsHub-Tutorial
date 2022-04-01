2e7de75e82388248832f0c3f3184f565082b4f81
DARK-art108 <daydreamingguy941@gmail.com>
2022-04-01 12:25:10 +0530

    Modified Data and Outputs

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from sklearn.metrics import roc_auc_score, average_precision_score, accuracy_sco
     f1_score
 import joblib
 from sklearn.model_selection import train_test_split
+import argparse
+import warnings
+warnings.filterwarnings('ignore')
 
+CLASS_LABEL = 'MachineLearning'
+train_df_path = 'data/train.csv.zip'
+test_df_path = 'data/test.csv.zip'
 
 def feature_engineering(raw_df):
     df = raw_df.copy()
@@ -28,11 +34,6 @@ def fit_tfidf(train_df, test_df):
     test_tfidf = tfidf.transform(test_df['Text'])
     return train_tfidf, test_tfidf, tfidf
 
-
-# def fit_model(train_X, train_y):
-#     clf_tfidf = LogisticRegression(solver='sag')
-#     clf_tfidf.fit(train_X, train_y)
-#     return clf_tfidf
 def fit_model(train_X, train_y, random_state=42):
     clf_tfidf = SGDClassifier(loss='modified_huber', random_state=random_state)
     clf_tfidf.fit(train_X, train_y)
@@ -51,30 +52,54 @@ def eval_model(clf, X, y):
         'f1': f1_score(y, y_pred),
     }
 
-
-if __name__ == '__main__':
+def split(random_state=42):
     print('Loading data...')
     df = pd.read_csv('data/CrossValidated-Questions.csv')
-    train_df, test_df = train_test_split(df)
-    del df
+    df[CLASS_LABEL] = df['Tags'].str.contains('machine-learning').fillna(False)
+    train_df, test_df = train_test_split(df, random_state=random_state, stratify=df[CLASS_LABEL])
 
+    print('Saving split data...')
+    train_df.to_csv(train_df_path)
+    test_df.to_csv(test_df_path)
+
+def train():
+    print('Loading data...')
+    train_df = pd.read_csv(train_df_path)
+    test_df = pd.read_csv(test_df_path)
+
+    print('Engineering features...')
     train_df = feature_engineering(train_df)
     test_df = feature_engineering(test_df)
 
     print('Fitting TFIDF...')
     train_tfidf, test_tfidf, tfidf = fit_tfidf(train_df, test_df)
 
-    print('Fitting classifier...')
-    train_y = train_df['MachineLearning']
+    print('Saving TFIDF object...')
+    joblib.dump(tfidf, 'outputs/tfidf.joblib')
+
+    print('Training model...')
+    train_y = train_df[CLASS_LABEL]
     model = fit_model(train_tfidf, train_y)
 
     print('Saving trained model...')
     joblib.dump(model, 'outputs/model.joblib')
 
+    print('Evaluating model...')
     train_metrics = eval_model(model, train_tfidf, train_y)
     print('Train metrics:')
     print(train_metrics)
 
-    test_metrics = eval_model(model, test_tfidf, test_df['MachineLearning'])
+    test_metrics = eval_model(model, test_tfidf, test_df[CLASS_LABEL])
     print('Test metrics:')
     print(test_metrics)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='Split or Train step:', dest='step')
+    subparsers.required = True
+    split_parser = subparsers.add_parser('split')
+    split_parser.set_defaults(func=split)
+    train_parser = subparsers.add_parser('train')
+    train_parser.set_defaults(func=train)
+    parser.parse_args().func()
